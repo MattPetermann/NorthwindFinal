d49da0edb21a5b1e8ae8a5a91fb87a7702e771ef
MATTPC\Matt <matthewp214@gmail.com>
2019-04-23 15:15:14 -0500

    Initial commit

---
+++ source/Northwind/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+		{
+			ViewBag.Message = "15% Off Today!";
+
+			return View();
+        }
+
+		public ActionResult Order()
+		{
+			ViewBag.Message = "Place an Order";
+
+			return View();
+		}
+
+        public ActionResult Products()
+		{
+			ViewBag.Message = "Your product page.";
+
+			return View();
+        }
+
+        public ActionResult Account()
+        {
+            ViewBag.Message = "Your account page.";
+
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            ViewBag.Message = "Your cart page.";
+
+            return View();
+        }
+    }
+}
---
+++ source/Northwind/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Northwind.Models
+{
+    public class Product
+    {
+        public int Id { get; }
+        public double Price { get; }
+        public string Name { get; }
+		public string Description { get; }
+		public string ImgUrl { get; }
+
+        public Product(int id, double price, string name, string description, string img)
+        {
+            Id = id;
+            Price = price;
+            Name = name;
+			Description = description;
+			ImgUrl = img;
+        }
+    }
+}
