e44c1555729789207d006593ef5c384e0e409faa
Matt Petermann <MPetermann2@my.wctc.edu>
2019-05-07 19:35:31 -0500

    Crud

--- Lesson 26/Northwind/Controllers/CartController.cs
+++ Lesson 26/Northwind/Controllers/CartController.cs
@@ -54,6 +54,89 @@ namespace Northwind.Controllers
             }
         }
 
+        // POST: Cart/DeleteCart
+        [HttpPost]
+        public JsonResult DeleteCart(CartDTO cartDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+                return Json(new { }, JsonRequestBehavior.AllowGet);
+            }
+
+            Cart sc = new Cart();
+            sc.CustomerID = cartDTO.CustomerID;
+
+            using (NorthwndEntities db = new NorthwndEntities())
+            {
+                Cart cart = db.Carts.SingleOrDefault(c => c.ProductID == cartDTO.ProductID 
+                                                        && c.CustomerID == cartDTO.CustomerID);
+                if (cart != null)
+                {
+					// cart exists
+					db.Carts.Remove(cart);
+                }
+
+                db.SaveChanges();
+                return Json(sc, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // POST: Cart/ClearCart
+        [HttpPost]
+        public JsonResult ClearCart(CartDTO cartDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+                return Json(new { }, JsonRequestBehavior.AllowGet);
+            }
+
+            Cart sc = new Cart();
+            sc.CustomerID = cartDTO.CustomerID;
+
+            using (NorthwndEntities db = new NorthwndEntities())
+            {
+                List<Cart> carts = db.Carts.Where(c => c.CustomerID == cartDTO.CustomerID).ToList();
+                if (carts != null)
+                {
+					foreach (Cart c in carts)
+						db.Carts.Remove(c);
+                }
+
+                db.SaveChanges();
+                return Json(sc, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // POST: Cart/EditCart
+        [HttpPost]
+        public JsonResult EditCart(CartDTO cartDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+                return Json(new { }, JsonRequestBehavior.AllowGet);
+            }
+
+            Cart sc = new Cart();
+            sc.CustomerID = cartDTO.CustomerID;
+
+            using (NorthwndEntities db = new NorthwndEntities())
+            {
+                Cart cart = db.Carts.SingleOrDefault(c => c.ProductID == cartDTO.ProductID 
+                                                        && c.CustomerID == cartDTO.CustomerID);
+                if (cart != null)
+                {
+                    // cart exists
+                    cart.Quantity = cartDTO.Quantity;
+                }
+
+                db.SaveChanges();
+                return Json(sc, JsonRequestBehavior.AllowGet);
+            }
+        }
+
 		// POST: Cart/ViewCart
 		[HttpPost]
 		public JsonResult ViewCart(CartDTO cartDTO)
@@ -63,12 +146,13 @@ namespace Northwind.Controllers
 
 			using (NorthwndEntities db = new NorthwndEntities())
 			{
-				var carts = db.Carts.Where(c => c.CustomerID == cartDTO.CustomerID).Select(
-					c => new
+				var carts = db.Carts.Where(c => c.CustomerID == cartDTO.CustomerID)
+					.Join(db.Products, c => c.ProductID, p => p.ProductID, 
+					(c, p) => new
 					{
-						//Change to qty and product id vs product table
-						c.CustomerID,
-						c.CartID
+						c.Quantity,
+						c.ProductID,
+						p.ProductName
 					}).ToList();
 
 				return Json(carts, JsonRequestBehavior.AllowGet);
